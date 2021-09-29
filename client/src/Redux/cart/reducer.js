6747c702f47c8d765998d04504933c483c93ca21
Shourov Saha <shourovys@gmail.com>
2021-09-29 22:34:46 +0600

    second time product added Handled in cart bug fixed

--- client/src/Redux/cart/reducer.js
+++ client/src/Redux/cart/reducer.js
@@ -7,21 +7,28 @@ import { ADD_TO_CART } from "./actions";
 const initialState = [];
 
 export default function cartReducer(state = initialState, action) {
-
   switch (action.type) {
     case ADD_TO_CART:
       //checking is this product is precisely added in cart
       // if added then update the quantity of that product (privies quantity + new quantity)
-     const updatedState = state.map((cartProduct) =>
-        action.payload._id === cartProduct._id
-          ? {
-              ...cartProduct,
-              quantity: cartProduct.quantity + action.payload.quantity,
-            }
-          : cartProduct
+
+      const isPreviouslyPresent = state.find(
+        (cartProduct) => cartProduct._id === action.payload._id
       );
-      setItemInLocalStorage("cart", updatedState);
-      return updatedState;
+      if (isPreviouslyPresent) {
+        const updatedState = state.map((cartProduct) =>
+          action.payload._id === cartProduct._id
+            ? {
+                ...cartProduct,
+                quantity: cartProduct.quantity + action.payload.quantity,
+              }
+            : cartProduct
+        );
+        setItemInLocalStorage("cart", updatedState);
+        return updatedState;
+      }
+      setItemInLocalStorage("cart", [action.payload, ...state]);
+      return [action.payload, ...state];
 
     default:
       return getItemFromLocalStorage("cart");
