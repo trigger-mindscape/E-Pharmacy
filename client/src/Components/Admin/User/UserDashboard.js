371c77f65e3509735c817ab9607f493bd6963b08
amir <iamamirmostafa@gmail.com>
2021-09-30 20:04:51 +0600

    update active order

--- client/src/Components/Admin/User/UserDashboard.js
+++ client/src/Components/Admin/User/UserDashboard.js
@@ -3,12 +3,14 @@ import NavBar from '../../Home/NavBar/NavBar';
 import PreviousOrders from './PreviousOrders/PreviousOrders';
 import UserProfile from './UserProfile/UserProfile';
 import Footer from '../../Home/Footer/Footer'
+import ActiveOrder from './ActiveOrder/ActiveOrder';
 
 const UserDashboard = () => {
   return (
     <>
       <NavBar/>
       <UserProfile/>
+      <ActiveOrder/>
       <PreviousOrders/>
       <Footer/>
     </>
